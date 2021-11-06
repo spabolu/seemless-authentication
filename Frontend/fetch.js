11c1fa1ff557e534f2a862f71985341f8b9ef3a0
Saketh Pabolu <51687055+spabolu@users.noreply.github.com>
2021-11-06 16:49:41 -0700

    changed the `fetch.js` and merging website files

--- Frontend/fetch.js
+++ Frontend/fetch.js
@@ -1,52 +1,42 @@
-const url = 'https://pokeapi.co/api/v2/pokemon/1'
+const postE = document.getElementById('post-event');
+const postD = document.getElementById('post-dining');
 
-const getBtn = document.getElementById('get-btn');
-const postBtn = document.getElementById('post-btn');
+const url = 'https://kfpdqk0mz7.execute-api.us-west-2.amazonaws.com';
 
-const getData = () => {
-    axios.get("https://pokeapi.co/api/v2/pokemon/1")
-        .then(response => {
-            console.info(response)
-            console.info("Pokemon Name: " + response.data.name);
-        })
-        .catch(err => {
-            console.error(err);
-        })
-};
+function onScanSuccess(decodedText, decodedResult) {
+    if (decodedText !== lastResult) {
+        lastResult = decodedText;
+        
+        console.log(`Code matched = ${decodedText}`, decodedResult);
+    }
+}
 
-// const sendData = () => {
-//     axios.post(
-//         'https://kfpdqk0mz7.execute-api.us-west-2.amazonaws.com/checkDiningHallBalance',
-//         {
-//             "userHash": "4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce",
-//             "EventName": "Football"
-//         },
-//         {
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Access-Control-Allow-Origin': '*'
-//             }
-//         }
-//     )
-//         .then(response => {
-//             //console.log(response.data);
-//             if (response.data[0].E === 200) {
-//                 console.log("Name: " + response.data[0].Name);
-//                 console.log("Meal Swipes: " + response.data[0].SwipesLeft);
-//                 //console.log(response.data[0].CORONA);
-//             }
-//         })
-//         .catch(err => {
-//             console.log(err, err.response);
-//         });
-// };
+let html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader",
+    { fps: 10, qrbox: { width: 250, height: 250 } },
+/* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess);
 
-const sendData = () => {
-    axios.post(
-        'https://kfpdqk0mz7.execute-api.us-west-2.amazonaws.com/allowedToEnter',
+
+async function sha256(ID) {
+    // hash the message AND encode as UTF-8
+    const hashBuffer = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(ID));
+
+    // convert ArrayBuffer to Array
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    // convert bytes to hex string                  
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+    return hashHex;
+}
+
+const sendDining = () => {
+
+    axios.post(url +
+        '/checkDiningHallBalance',
         {
-            "userHash": "4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce",
-            "event": "Club Event" // Sport Event, Club Event, SDFC
+            "userHash": "4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce"
         },
         {
             headers: {
@@ -56,16 +46,12 @@ const sendData = () => {
         }
     )
         .then(response => {
-            console.log(response.data[0]);
-            console.log("Name: " + response.data[0].name);
+            // console.log(response);
+            console.log("Location: Barrett's Dining Hall\n")
             if (response.data[0].E === 200) {
-                if (response.data[0].Vaccinated == true) {
-                    console.info("Access Allowed!")
-                } else {
-                    console.info("You don't fulfill the event-access requirments to enter in.");
-                }
-            } else if (response.data[0].E === 407) {
-                console.log("Don't have access to the event.")
+                console.log("Name: " + response.data[0].Name);
+                console.log("Meal Swipes Left: " + response.data[0].SwipesLeft);
+                console.log("\n Welcome! Today's special includes Chicken Parmesan, Paneer Masala and Poke Bowls")
             }
         })
         .catch(err => {
@@ -73,5 +59,41 @@ const sendData = () => {
         });
 };
 
-getBtn.addEventListener('click', getData);
-postBtn.addEventListener('click', sendData);
+const sendEvent = () => {
+    let eventType = 'Club Event'
+    axios.post(url +
+        '/allowedToEnter',
+        {
+            "userHash": "4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce",
+            "event": eventType // Sport Event, Club Event, SDFC
+        },
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+    )
+        .then(response => {
+            // console.log(response);
+            console.log('Welcome to ASU ' + eventType + '\n')
+            console.log("Name: " + response.data[0].name);
+            if (response.data[0].E === 200) { // 200 code stands for successful response
+                if (response.data[0].Vaccinated == true) { // checking if the user is vaccinated or not
+                    console.info("Access Allowed. Enjoy your event!")
+                } else {
+                    console.info("You don't fulfill the event requirments to enter in. Please check your MY ASU app for more information.");
+                }
+            } else if (response.data[0].E === 407) { // 407 code stands for not allowed into this specific-event
+                console.log("Don't have access to this event. Check the front desk.")
+            } else {
+                console.log("User not found. Please try again or check with the front desk.")
+            }
+        })
+        .catch(err => {
+            console.log(err, err.response);
+        });
+};
+
+postE.addEventListener('click', sendDining);
+postD.addEventListener('click', sendEvent);
