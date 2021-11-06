cfacaeda92eb206f62bd4b6529d480bdaa41707b
Saketh Pabolu <51687055+spabolu@users.noreply.github.com>
2021-11-06 15:36:07 -0700

    implementing qr code library

--- Frontend/fetch.js
+++ Frontend/fetch.js
@@ -43,10 +43,10 @@ const getData = () => {
 
 const sendData = () => {
     axios.post(
-        'https://kfpdqk0mz7.execute-api.us-west-2.amazonaws.com/allowToEnter',
+        'https://kfpdqk0mz7.execute-api.us-west-2.amazonaws.com/allowedToEnter',
         {
             "userHash": "4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce",
-            "EventName": "Football"
+            "event": "Club Event" // Sport Event, Club Event, SDFC
         },
         {
             headers: {
@@ -56,11 +56,16 @@ const sendData = () => {
         }
     )
         .then(response => {
-            //console.log(response.data);
+            console.log(response.data[0]);
+            console.log("Name: " + response.data[0].name);
             if (response.data[0].E === 200) {
-                // console.log("Name: " + response.data[0].Name);
-                // console.log("Meal Swipes: " + response.data[0].SwipesLeft);
-                console.log("POST Success, baby!")
+                if (response.data[0].Vaccinated == true) {
+                    console.info("Access Allowed!")
+                } else {
+                    console.info("You don't fulfill the event-access requirments to enter in.");
+                }
+            } else if (response.data[0].E === 407) {
+                console.log("Don't have access to the event.")
             }
         })
         .catch(err => {
